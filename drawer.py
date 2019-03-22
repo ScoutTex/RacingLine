1357aa51643bf6b1dfca0a4028876d5ac11c53b9
ScoutTex <sscoutxx@gmail.com>
2019-03-22 09:46:29 +0800

    init drawer

--- drawer.py
+++ drawer.py
@@ -7,7 +7,17 @@ def main():
         print('try \'python drawer.py xxx.tk xxx.rl\'')
         return
     
+    track = argv[0]
+    line = argv[1]
 
+    if track[-3:0] != '.tk':
+        print('ERROR: wrong track file format, should be *.tk')
+        return
+    if line[-3:0] != '.rl':
+        print('ERROR: wrong racingline file format, should be *.rl')
+        return
+    
+    
 
 
 if __name__ == '__main__':
