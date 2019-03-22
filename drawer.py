b3f588b473d63a100f46e9382f47a4d0ae25bed2
ScoutTex <sscoutxx@gmail.com>
2019-03-22 09:41:02 +0800

    add drawer

---
+++ drawer.py
@@ -0,0 +1,14 @@
+import turtle as t
+from sys import argv
+
+def main():
+    if len(argv) != 2:
+        print('ERROR: no arguments given')
+        print('try \'python drawer.py xxx.tk xxx.rl\'')
+        return
+    
+
+
+
+if __name__ == '__main__':
+    main()
