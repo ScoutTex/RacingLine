875e3bc2076e951831159141927f6c064517c413
ScoutTex <sscoutxx@gmail.com>
2019-03-28 16:28:11 +0800

    test if everything work

--- tester.py
+++ tester.py
@@ -2,5 +2,8 @@ from track import Track
 from drawer import Drawer
 from racingline import Racingline
 
-d = Drawer(Track('tracks/simple_straight/simple_straight.tk'), [Racingline('tracks/simple_straight/rl01.rl')])
-d.draw()
+# d = Drawer(Track('tracks/simple_straight/simple_straight.tk'), [Racingline('tracks/simple_straight/rl01.rl')])
+# d.draw()
+tk = Track('tracks/simple_straight/simple_straight.tk')
+rl = Racingline('tracks/simple_straight/rl01.rl')
+print(tk.race(rl))
