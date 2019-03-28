0ad75147f89483446af039b17431bfe196937225
ScoutTex <sscoutxx@gmail.com>
2019-03-28 10:31:09 +0800

    init velocity

--- vel.py
+++ vel.py
@@ -1,10 +1,10 @@
 class Vel:  # velocity
     rate = 0
     angle = 0
-    
+
     def __init__(self, rate=0, angle=0):
         self.rate = rate
         self.angle = angle
-    
+
     def __str__(self):
         return "Vel(%.2f, %.2f)" % (self.rate, self.angle)
