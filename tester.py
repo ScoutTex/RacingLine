d35e35660356e78c822f2c58f2c454795bd17981
ScoutTex <sscoutxx@gmail.com>
2019-03-28 15:27:44 +0800

    test if drawer work

--- tester.py
+++ tester.py
@@ -1,5 +1,6 @@
-from track import *
-from drawer import *
+from track import Track
+from drawer import Drawer
+from racingline import Racingline
 
-d = Drawer('tracks/simple_straight/simple_straight.tk', ['tracks/simple_straight/rl01.rl'])
+d = Drawer(Track('tracks/simple_straight/simple_straight.tk'), [Racingline('tracks/simple_straight/rl01.rl')])
 d.draw()
