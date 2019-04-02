5f69a0e16c64ee211f765dcc54478072627808da
ScoutTex <sscoutxx@gmail.com>
2019-04-02 14:50:53 +0800

    tester

--- tester.py
+++ tester.py
@@ -9,6 +9,13 @@ from racingline import Racingline
  
 # d = Drawer(Track('tracks/simple_straight/simple_straight.tk'), [Racingline('tracks/simple_straight/rl01.rl')])
 # d.draw()
-tk = Track('tracks/simple_straight/simple_straight.tk')
-rl = Racingline('tracks/simple_straight/rl01.rl')
-print(tk.race(rl))
+
+# tk = Track('tracks/simple_straight/simple_straight.tk')
+# rl = Racingline('tracks/simple_straight/rl01.rl')
+# print(tk.race(rl))
+
+r = Rct(200,100,300,300)
+p0 = Pos(100,200)
+p1 = Pos(300, 1)
+
+print(r.crush(p0,p1))
