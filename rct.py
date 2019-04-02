57a4d887f445ada28c7420254d195b9f10a8e0b8
ScoutTex <sscoutxx@gmail.com>
2019-04-02 14:51:42 +0800

    add crush(Pos, Pos), if a Rct and a Segment intersect or not

--- rct.py
+++ rct.py
@@ -1,4 +1,5 @@
 from pos import Pos
+from vel import Vel
 
 
 class Rct(Pos):  # rectangle
@@ -19,3 +20,30 @@ class Rct(Pos):  # rectangle
 
     def is_on_edge(self, p):
         return p.is_on_edge(self)
+
+    def crush_hline(self, hp0, hp1, p0, p1):
+        if p0.bot == p1.bot:
+            return (p0.bot == hp0.bot
+                    and p0.left <= hp1.left
+                    and p1.left >= hp0.left)
+        x = p0.left + (hp0.bot - p0.bot) * (p1.left - p0.left) / (p1.bot - p0.bot)
+        return (x >= hp0.left and x <= hp1.left)
+
+    def crush_vline(self, vp0, vp1, p0, p1):
+        hp0 = Pos(vp0.bot, vp0.left)
+        hp1 = Pos(vp1.bot, vp1.left)
+        p0_ = Pos(p0.bot, p0.left)
+        p1_ = Pos(p1.bot, p1.left)
+        return self.crush_hline(hp0, hp1, p0_, p1_)
+
+    def crush(self, p0, p1):
+        if type(p1) == Vel:
+            p1 = p0 + p1
+        pld = Pos(self.left, self.bot)
+        prd = Pos(self.left + self.width, self.bot)
+        plu = Pos(self.left, self.bot + self.height)
+        pru = Pos(self.left + self.width, self.bot + self.height)
+        return (self.crush_hline(pld, prd, p0, p1)
+                or self.crush_hline(plu, pru, p0, p1)
+                or self.crush_vline(pld, plu, p0, p1)
+                or self.crush_vline(prd, pru, p0, p1))
