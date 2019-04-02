d0efd163655be547a63fd36c18cab0cae9628b7b
ScoutTex <sscoutxx@gmail.com>
2019-04-02 14:02:49 +0800

    tester

--- tester.py
+++ tester.py
@@ -1,7 +1,12 @@
+from pos import Pos
+from vel import Vel
+from rct import Rct
 from track import Track
 from drawer import Drawer
 from racingline import Racingline
 
+
+ 
 # d = Drawer(Track('tracks/simple_straight/simple_straight.tk'), [Racingline('tracks/simple_straight/rl01.rl')])
 # d.draw()
 tk = Track('tracks/simple_straight/simple_straight.tk')
