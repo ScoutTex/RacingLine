9f48a0d74a3d3a9b4602ac35fc385ab9d327fb6f
ScoutTex <sscoutxx@gmail.com>
2019-05-20 08:37:29 +0800

    seperate evolution() and evolution_list(), change both ragte and angle in same step

--- evolutioner.py
+++ evolutioner.py
@@ -4,36 +4,43 @@ from random import randrange, uniform
 
 class Evolutioner:
     @staticmethod
+    def evolution_list(rls_raw, grow_ratio=5, max_d_rate=1, max_d_angle=10):
+        rls = []
+        for rl_raw in rls_raw:
+            rl_raw_evo = Evolutioner.evolution(rl_raw, grow_ratio, max_d_rate, max_d_angle)
+            for rl_raw_evo0 in rl_raw_evo:
+                rls.append(rl_raw_evo0)
+        return rls
+
+    @staticmethod
     def evolution(rl, grow_ratio=5, max_d_rate=1, max_d_angle=10):
         rls = []
         if type(rl) is list:
-            for rl0 in rl:
-                rl0_s = Evolutioner.evolution(rl0)
-                for rl0_s_ in rl0_s:
-                    rls.append(rl0_s_)
+            rls = Evolutioner.evolution_list(rl, grow_ratio, max_d_rate, max_d_angle)
         elif type(rl) is Racingline:
             rls.append(rl)
             for _ in range(grow_ratio):
                 rl1 = Racingline(rls[0])
+                rl2 = Racingline(rls[0])
                 change_step = randrange(len(rl.steps))
-                change_rate = bool(randrange(2))
-                if change_rate:
-                    rate = rl1.steps[change_step].rate
-                    rate += uniform(-max_d_rate, max_d_rate)
-                    # rate += randrange(-max_d_rate, max_d_rate, 1/1000)
-                    if rate > max_d_rate:
-                        rate = max_d_rate
-                    if rate < -max_d_rate:
-                        rate = -max_d_rate
-                    rl1.steps[change_step].rate = rate
-                else:  # change angle
-                    angle = rl1.steps[change_step].angle
-                    angle += uniform(-max_d_angle, max_d_angle)
-                    # angle += randrange(-max_d_angle, max_d_angle, 1/100)
-                    if angle > max_d_angle:
-                        angle = max_d_angle
-                    if angle < -max_d_angle:
-                        angle = -max_d_angle
-                    rl1.steps[change_step].angle = angle
+                # change both rate and angle in same step
+                # change_rate = bool(randrange(2))
+                # change rate
+                rate = rl1.steps[change_step].rate
+                rate += uniform(-max_d_rate, max_d_rate)
+                if rate > max_d_rate:
+                    rate = max_d_rate
+                if rate < -max_d_rate:
+                    rate = -max_d_rate
+                rl1.steps[change_step].rate = rate
                 rls.append(rl1)
+                # change angle
+                angle = rl2.steps[change_step].angle
+                angle += uniform(-max_d_angle, max_d_angle)
+                if angle > max_d_angle:
+                    angle = max_d_angle
+                if angle < -max_d_angle:
+                    angle = -max_d_angle
+                rl2.steps[change_step].angle = angle
+                rls.append(rl2)
         return rls
