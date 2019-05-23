5735440ab99b70704c1f4a5909acd8f70bad8e97
ScoutTex <sscoutxx@gmail.com>
2019-05-23 13:46:57 +0800

    change format to avoid flake8 warning

--- tester.py
+++ tester.py
@@ -1,12 +1,7 @@
-from pos import Pos
-from vel import Vel
-from rct import Rct
 from track import Track
 from drawer import Drawer
 from racingline import Racingline
 from evolutioner import Evolutioner
-
-
 
 # d = Drawer(Track('tracks/simple_straight/simple_straight.tk'), [Racingline('tracks/simple_straight/rl01.rl')])
 # d.draw()
@@ -14,7 +9,7 @@ from evolutioner import Evolutioner
 tk = Track('tracks/simple_straight/simple_straight.tk')
 rl = Racingline('tracks/simple_straight/rl01.rl')
 # print(tk.race(rl))
-# 
+#
 # d = Drawer(tk, [rl])
 # d.draw()
 
